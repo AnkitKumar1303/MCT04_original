de1f78cbbd1534c22a6d6b7062631afd6ef44c2f
Ankit Kumar <42855900+AnkitKumar1303@users.noreply.github.com>
2023-11-18 02:24:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/file",(req,res)=>{
-    res.sendFile(path.join(_dirname,'/index.html'));
+    res.sendFile(path.join(__dirname,'/public'));
 });
 app.get("/",(req,res)=>{
     res.send('Hiii');
